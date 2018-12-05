f328930477301b14ac55e6a4d28e062117a1c8a8
Guray Gursoy <GURSOGU2@novartis.net>
2018-12-05 12:31:13 +0100

    add javascript

---
+++ shared.js
@@ -0,0 +1,38 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButton = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+for (var i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = "open"; //this will override the classlist
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
+}
+
+backdrop.addEventListener("click", function() {
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if(modalNoButton){
+modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+    if(modal){
+        modal.classList.remove("open");
+    }
+
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function() {
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
